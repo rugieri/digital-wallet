f6255089dd4c083c0b3f6f743c59a30ae41b9dc8
rugieri <rugieri.ronaldo@gmail.com>
2021-08-30 21:47:30 -0300

    created and integrated signup-complete tests

--- signup/src/ports/repos/signup.ts
+++ signup/src/ports/repos/signup.ts
@@ -4,6 +4,16 @@ const insert = async (_signup: Signup): Promise<void> => {
   throw new Error("Not implemented yet");
 };
 
+const updateStatus = async (_signup: Signup, _newStatus: string): Promise<Signup> => {
+  throw new Error("Not implemented yet");
+};
+
+const getByToken = async (_token: string): Promise<Signup> => {
+  throw new Error("Not implemented yet");
+};
+
 export default {
   insert,
+  updateStatus,
+  getByToken,
 };
