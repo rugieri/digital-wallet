88ca15bb4b3c9cac5c8b3c74beaaea5d0d70cecd
rugieri <rugieri.ronaldo@gmail.com>
2021-09-15 18:18:46 -0300

    added sign up notification

--- signup/test/component/signup-complete.test.ts
+++ signup/test/component/signup-complete.test.ts
@@ -3,14 +3,17 @@ import signupComplete from "@src/controllers/signup-complete";
 import { SinonStub, stub, restore } from "sinon";
 import signupRepo from "@src/ports/repos/signup";
 import Signup from "src/types/singup";
+import signupNotifier from "@src/ports/notifiers/signup";
 
 let getByTokenSignup: SinonStub;
 let updateStatusSignup: SinonStub;
+let completeNotificationSignup: SinonStub;
 
 describe("Signup complete", () => {
   beforeEach(() => {
     getByTokenSignup = stub(signupRepo, "getByToken");
     updateStatusSignup = stub(signupRepo, "updateStatus");
+    completeNotificationSignup = stub(signupNotifier, "complete");
   });
 
   afterEach(() => restore());
@@ -21,6 +24,13 @@ describe("Signup complete", () => {
     await signupComplete(token);
 
     expect(updateStatusSignup).to.have.been.calledWith(signup, "COMPLETE");
+  });
+  it("send notification when signup is completed", async () => {
+    const token = "some-token";
+    getByTokenSignup.resolves(signup);
+    await signupComplete(token);
+
+    expect(completeNotificationSignup).to.have.been.calledOnce;
   });
 });
 
